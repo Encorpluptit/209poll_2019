23646f258854df916193bc34c2cd6cf88756ad30
BERNARD Damien <damien.bernard@epitech.eu>
2020-05-11 21:26:53 +0200

    add(tests): Adding beginning of python unit tests and jenrik binary.

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+##
+## EPITECH PROJECT, 2020
+## 209Polls
+## File description:
+## Source file for Python tests.
+##
+
+from Poll import Poll
+
+module_poll = __import__("209poll")
