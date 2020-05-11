338ec39532375145e9ca77788a5efc3a340ef04a
BERNARD Damien <damien.bernard@epitech.eu>
2020-05-11 21:23:37 +0200

    fix(python): Fixing print help.

--- Poll.py
+++ Poll.py
@@ -19,22 +19,25 @@ class Poll:
     @staticmethod
     def print_help(exit_code: int = 0):
         print(
-            "Usage\n" +
-            f"\t{argv[0]}" +
-            ""
-              )
+            "Usage\n"
+            f"\t{argv[0]} pSize sSize p\n\n"
+            "DESCRIPTION:\n"
+            "\tpSize\tsize of the population\n"
+            "\tsSize\tsize of the sample (supposed to be representative)\n"
+            "\tp\t\tpercentage of voting intentions for a specific candidate"
+        )
         exit(exit_code)
 
     def calc_variance(self):
         self.var = ((self.p / 100) * (1 - (self.p / 100)) / self.s_size) * (
-                    (self.p_size - self.s_size) / (self.p_size - 1))
-        self.ci95 = 196 * sqrt(self.var)
-        self.ci99 = 258 * sqrt(self.var)
+                (self.p_size - self.s_size) / (self.p_size - 1))
+        self.ci95 = 1.96 * sqrt(self.var) * 100
+        self.ci99 = 2.58 * sqrt(self.var) * 100
 
     def print_results(self):
-        print(f"Population Size:\t\t{self.p_size}")
-        print(f"Sample Size:\t\t\t{self.s_size}")
+        print(f"Population size:\t\t{self.p_size}")
+        print(f"Sample size:\t\t\t{self.s_size}")
         print(f"Voting intentions:\t\t{self.p}%")
-        print(f'Variance:\t\t\t\t{self.var:.6f}')
-        print(f"95% confidence interval: [{max(self.p - self.ci95, 0):.2f}%; {min(self.p + self.ci95, 100):.2f}%]")
-        print(f"99% confidence interval: [{max(self.p - self.ci99, 0):.2f}%; {min(self.p + self.ci99, 100):.2f}%]")
+        print(f'Variance:\t\t\t{self.var:.6f}')
+        print(f"95% confidence interval:\t[{max(self.p - self.ci95, 0):.2f}%; {min(self.p + self.ci95, 100):.2f}%]")
+        print(f"99% confidence interval:\t[{max(self.p - self.ci99, 0):.2f}%; {min(self.p + self.ci99, 100):.2f}%]")
